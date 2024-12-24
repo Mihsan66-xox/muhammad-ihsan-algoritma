827bc6941af67770e7f830a1a79cb7aec859ad83
Mihsan66-xox <sanajaa66@gmail.com>
2024-12-24 11:03:43 +0800

    Delete modul 5 directory

--- modul 5/14-LatihanPercabangan/main.py
+++
@@ -1,7 +0,0 @@
-belanjaan = int(input("masukan total belanjaan : "))
-if belanjaan > 500000 :
-    print("anda mendapatkan mouse dan keyboard")
-else:
-    print("belanjaan anda kurang dari 50000")
-    print("anda tidak mendapatkan moouse dan keybord")
-print("terimakasih")
